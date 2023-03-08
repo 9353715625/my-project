d567fcfeb912d9a4fe707470fc3a6b643e8d5ba9
9353715625 <127285738+9353715625@users.noreply.github.com>
2023-03-08 12:41:44 +0530

    Add files via upload

---
+++ example.java
@@ -0,0 +1,9 @@
+
+//Example:sum of two number
+public class example{
+public static void main(string[]args){
+ int num1=5,num2=15,sum;
+  sum=num1+num2;
+   system.outprintin("sum of two numbers is:"5+15):
+}
+}
