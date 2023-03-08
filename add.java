a4c025f97636e71f1d55e1f730ae1f21d5a994d4
9353715625 <127285738+9353715625@users.noreply.github.com>
2023-03-08 12:29:23 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,15 @@
+public class add
+{
+public static void main(String args[])
+{
+int first = 12,second = 5,add,subtract,multiply,divide;
+add = first + second;
+subtract = first - second;
+multiply = first * second;
+divide = first / second;
+System.out.println("sum="+add);
+System.out.println("subtraction="+subtract);
+System.out.println("multiplication="+multiply);
+System.out.println("division="+divide);
+}
+}
